d09d4e86278eda743ee42e259c799eec6398abf9
AmirHossein-nr <arazlighi@gmail.com>
2024-04-19 16:14:55 +0330

    add documentation

--- Logic/core/__init__.py
+++ Logic/core/__init__.py
@@ -1,16 +1,7 @@
-import json
-import preprocess
-import LSH
+from .indexer import *
+from .language_model import *
+from .utility import *
+from .search import *
 
-documents = []
-with open("LSHFakeData.json", 'r') as f:
-    documents = json.load(f)
-summaries = []
-for doc in documents:
-    summaries.append(doc['summaries'][0])
 
-pre = preprocess.Preprocessor(summaries)
-pre_documents = pre.preprocess()
-lsh = LSH.MinHashLSH(pre_documents,100)
-buckets = lsh.perform_lsh(number_of_bands=20,number_of_rows=5,number_of_buckets=50)
-lsh.jaccard_similarity_test(buckets,pre_documents)
+__all__ = [k for k in globals().keys() if not k.startswith("_")]
